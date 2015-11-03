c241183dace089779dc01698a21d58773203e5ac
CallAnotherBarry <computermaniac9999@gmail.com>
2015-11-03 02:18:42 -0600

    MODEL ROTATION FIX

--- Website/Threejs/js/main.js
+++ Website/Threejs/js/main.js
@@ -1,15 +1,19 @@
 // Set up the scene, camera, and renderer as global variables.
 var scene, camera, renderer;
-
 init();
 animate();
+function test() {
+	alert('hello world1');
+}
+
 
 // Sets up the scene.
 function init() {
+
   // Create the scene and set the scene size.
   scene = new THREE.Scene();
   var WIDTH = 300;
-      HEIGHT = 500;
+      HEIGHT = 400;
 container = document.getElementById("threeModel");
 
 //document.body.appendChild( container );
@@ -20,12 +24,14 @@ container = document.getElementById("threeModel");
   console.log("Appended to the child!");
   renderer.domElement.id = "context"
 
+
   
   // Create a camera, zoom it out from the model a bit, and add it to the scene.
  
   camera = new THREE.PerspectiveCamera(45, WIDTH / HEIGHT, 0.1, 300);
-  camera.position.set(0,10,0);
+  camera.position.set(0,0,4);
   scene.add(camera);
+  camera.lookAt(scene);
 
   // Create a light, set its position, and add it to the scene.
 
@@ -44,11 +50,13 @@ container = document.getElementById("threeModel");
     var material = new THREE.MeshLambertMaterial({color: 0xCCCCCC});
     var mesh = new THREE.Mesh( geometry, material);
     scene.add(mesh);
+    mesh.position.set(0, -1, 0);
+    
 
   }); 
 
   
-  
+ 
 
   
 
@@ -58,6 +66,7 @@ container = document.getElementById("threeModel");
 
 // Renders the scene and updates the render as needed.
 function animate() {
+
   requestAnimationFrame( animate );
   renderer.render( scene, camera );
   controls.update();
